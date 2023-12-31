3c3dc43d776c038731add622d5f54f69ac1e572f
kapishmalik <malikkapish@gmail.com>
2023-12-31 21:00:10 +0530

    small changes

--- resources/com/company/nestedlib/modules/Push/DockerPush.groovy
+++ resources/com/company/nestedlib/modules/Push/DockerPush.groovy
@@ -1,2 +1,9 @@
 DOCKER_IMAGE_NAME = "${CFG.'docker.DOCKER_ARTIFACTORY_URL'}${CFG.'docker.DOCKER_ARTIFACTORY_PATH'}:${CFG.'docker.IMAGE_TAG'}"
-sh "docker push $DOCKER_IMAGE_NAME"
+if (CFG.'use_podman')
+{
+    sh "podman push $DOCKER_IMAGE_NAME"
+}
+else
+{
+    sh "docker push $DOCKER_IMAGE_NAME"
+}
