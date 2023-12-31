22eb5caab2e90a3086eb6a53ab7ec9bba7a6eb8e
kapishmalik <malikkapish@gmail.com>
2023-12-31 20:07:32 +0530

    small change in pipeline

--- resources/com/company/nestedlib/modules/Login/Login.groovy
+++ resources/com/company/nestedlib/modules/Login/Login.groovy
@@ -1,4 +1,4 @@
 echo "Docker login command to artifactory ${CFG.'docker.DOCKER_ARTIFACTORY_URL'} using credentials ${CFG.'docker.DOCKER_ARTIFACTORY_CREDENTIAL_ID'}"
 withCredentials([usernamePassword(credentialsId: "${CFG.'docker.DOCKER_ARTIFACTORY_CREDENTIAL_ID'}", usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
-    sh "echo ${PASSWORD} | docker login -u ${USERNAME} --password-stdin ${CFG.'docker.DOCKER_ARTIFACTORY_URL'}"
+    sh "echo ${PASSWORD} | podman login -u ${USERNAME} --password-stdin ${CFG.'docker.DOCKER_ARTIFACTORY_URL'}"
 }
