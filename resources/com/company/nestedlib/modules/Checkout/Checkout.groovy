550d77f3badf6f5f0796901bcabfa8c06d39ec50
kapishmalik <malikkapish@gmail.com>
2023-12-31 13:23:31 +0530

    shell command to do git clone and checkout

--- resources/com/company/nestedlib/modules/Checkout/Checkout.groovy
+++ resources/com/company/nestedlib/modules/Checkout/Checkout.groovy
@@ -2,3 +2,5 @@ echo "Hello I am using MPL nested library in Checkout stage"
 echo "Checkout code base from repo "
 echo CFG.'git.url'
 echo CFG.'git.branch'
+sh git clone CFG.'git.url'
+sh git checkout CFG.'git.branch'
