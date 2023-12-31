5e68fafe8be6d12d742ece570eb4944a94275df9
kapishmalik <malikkapish@gmail.com>
2023-12-31 20:00:05 +0530

    few more changes

--- resources/com/company/nestedlib/modules/Login/Login.groovy
+++ resources/com/company/nestedlib/modules/Login/Login.groovy
@@ -1,3 +1,5 @@
 echo "Docker login command to artifactory ${CFG.'docker.DOCKER_ARTIFACTORY_URL'} using credentials ${CFG.'docker.DOCKER_ARTIFACTORY_CREDENTIAL_ID'}"
 DOCKER_ARTIFACTORY_CREDENTIALS = credentials("${CFG.'docker.DOCKER_ARTIFACTORY_CREDENTIAL_ID'}")
-sh "echo ${DOCKER_ARTIFACTORY_CREDENTIALS_PSW} | docker login -u ${DOCKER_ARTIFACTORY_CREDENTIALS_USR} --password-stdin ${CFG.'docker.DOCKER_ARTIFACTORY_URL'}"
+withCredentials([string(credentialsId: "${CFG.'docker.DOCKER_ARTIFACTORY_CREDENTIAL_ID'}", variable: 'DOCKER_ARTIFACTORY_CREDENTIALS')]) {
+    sh "echo ${DOCKER_ARTIFACTORY_CREDENTIALS_PSW} | docker login -u ${DOCKER_ARTIFACTORY_CREDENTIALS_USR} --password-stdin ${CFG.'docker.DOCKER_ARTIFACTORY_URL'}"
+}
